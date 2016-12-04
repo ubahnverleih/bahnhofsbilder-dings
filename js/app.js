86a9281a8a72f2cb6722559a325d084cd5633661
Constantin Müller <ubahnverleih@gmail.com>
2016-12-04 11:18:55 +0100

    init

---
+++ js/app.js
@@ -0,0 +1,67 @@
+$().ready(function () {
+    new App();
+});
+var App = (function () {
+    function App() {
+        var _this = this;
+        var query = "\n\t\t\tSELECT ?item ?itemLabel {\n\t\t\t\t?item wdt:P31 wd:Q728937.\n\t\t\t\t?item wdt:P17 wd:Q183\n\t\t\t\tSERVICE wikibase:label {\n\t\t\t\t\tbd:serviceParam wikibase:language \"de\", \"en\" .\n\t\t\t\t}\n\t\t\t}\n\t\t";
+        var url = "https://query.wikidata.org/bigdata/namespace/wdq/sparql?format=json&query=" + encodeURI(query);
+        $.get(url, function (res) {
+            var items = [];
+            res['results']['bindings'].forEach(function (qitem) {
+                var qid = qitem['item']['value'].split('/').slice(-1)[0];
+                var label = qitem['itemLabel']['value'];
+                items.push({ id: qid, label: label });
+            });
+            console.log(items.length);
+            _this.renderLines(items);
+        });
+        $('#stationList').hide();
+    }
+    App.prototype.renderLines = function (items) {
+        var _this = this;
+        items.forEach(function (item) {
+            console.log(item);
+            var listitem = $('<li>').html(item['label']);
+            listitem.click(function () { _this.loadStations(item['id']); });
+            $('#lineList').append(listitem);
+        });
+    };
+    App.prototype.loadStations = function (itemID) {
+        var _this = this;
+        console.log(itemID);
+        var query = "\n\t\tSELECT ?item ?itemLabel ?image {\n\t\t\t?item wdt:P81 wd:" + itemID + ".\n\t\t\tOPTIONAL {?item wdt:P18 ?image}\n\t\t\tSERVICE wikibase:label {\n\t\t\t\tbd:serviceParam wikibase:language \"de\", \"en\".\n\t\t\t}\n\t\t}";
+        var url = 'https://query.wikidata.org/bigdata/namespace/wdq/sparql?format=json&query=' + encodeURI(query);
+        $.get(url, function (res) {
+            var items = [];
+            res['results']['bindings'].forEach(function (qitem) {
+                var label = qitem['itemLabel']['value'];
+                var image = qitem['image'] ? qitem['image']['value'] : "https://upload.wikimedia.org/wikipedia/commons/a/a5/Camera-photo_Upload-240px.png";
+                items.push({ image: image, label: label });
+            });
+            console.log(items.length);
+            _this.renderStations(items);
+        });
+    };
+    App.prototype.renderStations = function (items) {
+        var _this = this;
+        $('#stationList').html('');
+        if (items.lenght == 0) {
+            $('#stationList').html('no items found');
+        }
+        $('#stationList').show();
+        items.forEach(function (item) {
+            console.log(item);
+            var listitem = $('<li>').html("<img src='" + item['image'] + "?width=400' class='stationImage' /><p class='stationTitle'>" + item['label'] + "</p>");
+            $('#stationList').append(listitem);
+        });
+        var closebutton = $('<p id="closeButton">close</p>');
+        closebutton.click(function () { _this.closeOverlay(); });
+        $('#stationList').append(closebutton);
+    };
+    App.prototype.closeOverlay = function () {
+        $('#stationList').hide();
+    };
+    return App;
+}());
+//# sourceMappingURL=app.js.map
---
+++ ts/index.ts
@@ -0,0 +1,85 @@
+/// <reference path="../dts/jquery.d.ts" />
+
+$().ready(()=>{
+	new App();
+})
+
+class App {
+	constructor() {
+		let query = `
+			SELECT ?item ?itemLabel {
+				?item wdt:P31 wd:Q728937.
+				?item wdt:P17 wd:Q183
+				SERVICE wikibase:label {
+					bd:serviceParam wikibase:language "de", "en" .
+				}
+			}
+		`
+		let url = "https://query.wikidata.org/bigdata/namespace/wdq/sparql?format=json&query=" + encodeURI(query);
+		$.get(url, (res)=>{
+			let items = [];
+			res['results']['bindings'].forEach((qitem)=>{
+				let qid = qitem['item']['value'].split('/').slice(-1)[0];
+				let label = qitem['itemLabel']['value'];
+				items.push({id: qid, label: label});
+			})
+			console.log(items.length);
+			this.renderLines(items);
+		})
+		$('#stationList').hide();
+	}
+
+	public renderLines(items: Object[]) {
+		items.forEach((item)=>{
+			console.log(item);
+			let listitem = $('<li>').html(item['label']);
+			listitem.click(() => {this.loadStations(item['id'])})
+			$('#lineList').append(listitem);
+		})
+	}
+
+	public loadStations(itemID) {
+		console.log(itemID);
+		let query = `
+		SELECT ?item ?itemLabel ?image {
+			?item wdt:P81 wd:`+itemID+`.
+			OPTIONAL {?item wdt:P18 ?image}
+			SERVICE wikibase:label {
+				bd:serviceParam wikibase:language "de", "en".
+			}
+		}`
+		let url = 'https://query.wikidata.org/bigdata/namespace/wdq/sparql?format=json&query=' + encodeURI(query);
+		$.get(url, (res)=>{
+			let items = [];
+			res['results']['bindings'].forEach((qitem)=>{
+				let label = qitem['itemLabel']['value'];
+				let image = qitem['image'] ? qitem['image']['value'] : "https://upload.wikimedia.org/wikipedia/commons/a/a5/Camera-photo_Upload-240px.png";
+				items.push({image: image, label: label});
+			})
+			console.log(items.length);
+			this.renderStations(items);
+		})
+	}
+
+	public renderStations(items) {
+		$('#stationList').html('');
+		if (items.lenght == 0)
+		{
+			$('#stationList').html('no items found');
+		}
+		$('#stationList').show();
+		items.forEach((item)=>{
+			console.log(item);
+			let listitem = $('<li>').html("<img src='"+ item['image'] +"?width=400' class='stationImage' /><p class='stationTitle'>" + item['label'] + "</p>");
+			//listitem.click(() => {this.loadStations(item['id'])})
+			$('#stationList').append(listitem);
+		})
+		let closebutton = $('<p id="closeButton">close</p>');
+		closebutton.click(()=>{this.closeOverlay()})
+		$('#stationList').append(closebutton);
+	}
+
+	public closeOverlay() {
+		$('#stationList').hide();
+	}
+}
